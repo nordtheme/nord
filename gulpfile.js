14337e9a28bebce628dd4e9e9cc53bdc224edc76
Arctic Ice Studio <development@arcticicestudio.com>
2016-09-04 16:13:53 +0200

    Create the project gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/*
++++++++++++++++++++++++++++++++++++++++++++
+title     Project Gulp File               +
+project   nord                            +
+version                                   +
+author    Arctic Ice Studio               +
+email     development@arcticicestudio.com +
+copyright Copyright (C) 2016              +
++++++++++++++++++++++++++++++++++++++++++++
+
+[Index]
+  > Configuration
+  > Imports
+  > Tasks
+      clean
+      clean-css
+      clean-documentation
+      compile-css-template
+      default
+
+[References]
+Gulp
+  (http://gulpjs.com)
+npmjs
+  (https://www.npmjs.com)
+*/
+
+"use strict";
